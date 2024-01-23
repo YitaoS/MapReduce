2c2be3e7598e904ec744fbd5ee37a629f7d09038
YYitaoS <ys386@duke.com>
2024-01-23 09:24:11 -0500

    first commit

---
+++ src/mr/coordinator.go
@@ -0,0 +1,155 @@
+package mr
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+)
+
+type Coordinator struct {
+	// Your definitions here.
+	CurrentStage      Stage
+	MapTaskChannel    chan *Task
+	ReduceTaskChannel chan *Task
+	TaskInfoMap       map[int]*TaskInfo
+	ReduceNum         int
+	UTID              int
+	mu                *sync.Mutex
+}
+
+func (c *Coordinator) MakeMapTasks(files []string) {
+	for _, fileName := range files {
+		utid := c.getNewUTID()
+		task := Task{
+			UTID:        utid,
+			Type:        MapType,
+			TargetFiles: []string{fileName},
+			ReduceNum:   c.ReduceNum,
+		}
+		c.MapTaskChannel <- &task
+		taskInfo := TaskInfo{
+			Status:   Waiting,
+			TaskAddr: &task,
+		}
+		c.TaskInfoMap[utid] = &taskInfo
+	}
+}
+
+func (c *Coordinator) getNewUTID() int {
+	c.UTID += 1
+	return c.UTID
+}
+
+// Your code here -- RPC handlers for the worker to call.
+
+// workers ask for new job
+func (c *Coordinator) PullTask(args *GetTaskArgs, reply *Task) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	switch c.CurrentStage {
+	case MapStage:
+		{
+			if len(c.MapTaskChannel) > 0 {
+				*reply = *<-c.MapTaskChannel
+				taskInfo, ok := c.TaskInfoMap[reply.UTID]
+				if !ok || taskInfo == (TaskInfo{}) || taskInfo.Status != Waiting {
+					fmt.Println("[Error]", reply, "does not exist in TaskInfoMap or Assigned to worker repeatedly!")
+					panic(1)
+				}
+				taskInfo.Status = Processing
+			} else {
+				reply.Type = WaitType
+				if c.checkStageOver() {
+					c.NextStage()
+				}
+			}
+		}
+	// case ReduceStage:
+	// 	{
+	// 		*reply = *<-c.ReduceTaskChannel
+	// 	}
+	default:
+		{
+			reply.Type = ExitType
+		}
+	}
+	return nil
+}
+
+func (c *Coordinator) checkStageOver() bool {
+	for _, taskInfo := range c.TaskInfoMap {
+		if taskInfo.Status != Done {
+			return false
+		}
+	}
+	return true
+}
+
+func (c *Coordinator) NextStage() {
+	switch c.CurrentStage {
+	case MapStage:
+		{
+			c.CurrentStage = ExitStage
+		}
+	case ReduceStage:
+		{
+			c.CurrentStage = ExitStage
+		}
+	}
+}
+
+// an example RPC handler.
+//
+// the RPC argument and reply types are defined in rpc.go.
+func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+	reply.Y = args.X + 1
+	return nil
+}
+
+// start a thread that listens for RPCs from worker.go
+func (c *Coordinator) server() {
+	rpc.Register(c)
+	rpc.HandleHTTP()
+	//l, e := net.Listen("tcp", ":1234")
+	sockname := coordinatorSock()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
+
+// main/mrcoordinator.go calls Done() periodically to find out
+// if the entire job has finished.
+func (c *Coordinator) Done() bool {
+	ret := false
+
+	// Your code here.
+
+	return ret
+}
+
+// create a Coordinator.
+// main/mrcoordinator.go calls this function.
+// nReduce is the number of reduce tasks to use.
+func MakeCoordinator(files []string, nReduce int) *Coordinator {
+	// Your code here.
+	c := Coordinator{
+		CurrentStage:      MapStage,
+		MapTaskChannel:    make(chan *Task, len(files)),
+		ReduceTaskChannel: make(chan *Task, nReduce),
+		TaskInfoMap:       make(map[int]TaskInfo, len(files)+nReduce),
+		ReduceNum:         nReduce,
+		UTID:              0,
+		mu:                new(sync.Mutex),
+	}
+
+	c.server()
+	return &c
+}
---
+++ src/mr/task.go
@@ -0,0 +1,38 @@
+package mr
+
+type Task struct {
+	UTID        int
+	Type        TaskType
+	TargetFiles []string
+	ReduceNum   int
+}
+
+type TaskInfo struct {
+	Status   TaskStatus
+	TaskAddr *Task
+}
+
+type TaskType int
+
+const (
+	MapType TaskType = iota
+	ReduceType
+	WaitType
+	ExitType
+)
+
+type TaskStatus int
+
+const (
+	Processing TaskStatus = iota
+	Waiting
+	Done
+)
+
+type Stage int
+
+const (
+	MapStage Stage = iota
+	ReduceStage
+	ExitStage
+)
